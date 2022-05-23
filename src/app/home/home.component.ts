a6aa93f98c00bf847239d7d82a6aeb413303043e
son14071999 <son1999tmgl@gmail.com>
2022-05-23 21:09:55 +0700

    combobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,8 +14,39 @@ export class HomeComponent implements OnInit {
     {ten:'quyet', gia:5, dongGia:true},
     {ten:'xoai', gia:6, dongGia:false},
   ]
+  public cities = [
+    {
+      city: 'Ha Noi',
+      district : [
+        'Hai Ba Trung',
+        'Gia Lam',
+        'Long Bien'
+      ]
+    },
+    {
+      city: 'Bac Ninh',
+      district : [
+        'Thuan thanh',
+        'TU Son',
+        'yen phong'
+      ]
+    },
+    {
+      city: 'HCM',
+      district : [
+        'Quan 1',
+        'QUan 9',
+        'Tan son nhat'
+      ]
+    }
+    
+  ]
+  public districts:string[] = []
   ngOnInit(): void {
-
+    this.districts = this.cities[0].district
+  }
+  changeCity(event:any){
+    this.districts=(this.cities.filter(city => city.city === event.target.value))[0].district
   }
 
 }
